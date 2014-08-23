2526eaac76d32f1317b9afff4e6d1c69a5526396
Chris Mondok <chris.mondok@gmail.com>
2014-08-22 23:56:07 -0400

    Pawns put themselves into arrays

--- Pawn.js
+++ Pawn.js
@@ -1,6 +1,16 @@
-function Pawn() {
+function Pawn(game) {
+	if(!game || !game instanceof Game)
+		throw new TypeError("Game isn't right.");
+
+	this.game = game;
+
+	this.game.pawns.push(this);
 }
 
 Pawn.prototype.tick = function(dt) {
 	//override this
+}
+
+Pawn.prototype.destructor = function() {
+	this.game.pawns.splice(this.game.pawns.indexOf(this), 1);
 }
