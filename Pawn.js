8d6305535c22c5b89a13a5acf3f0c47c04c05e70
Chris Mondok <chris.mondok@gmail.com>
2014-08-23 01:35:05 -0400

    Pawns have velocity and can receive forces

--- Pawn.js
+++ Pawn.js
@@ -1,16 +1,33 @@
-function Pawn(game) {
+function Pawn(game, x, y) {
 	if(!game || !game instanceof Game)
 		throw new TypeError("Game isn't right.");
 
+	if(typeof(x) != "number" || typeof(y) != "number")
+		throw new TypeError("You need to define x and y!");
+
 	this.game = game;
+	this.x = x;
+	this.y = y;
 
 	this.game.pawns.push(this);
+
+	this.velocity = {x: 0, y: 0};
 }
 
 Pawn.prototype.tick = function(dt) {
-	//override this
+	this.x += this.velocity.x;
+	this.y += this.velocity.y;
+}
+
+Pawn.prototype.draw = function(dt) {
+	//does nothing
 }
 
 Pawn.prototype.destructor = function() {
 	this.game.pawns.splice(this.game.pawns.indexOf(this), 1);
+}
+
+Pawn.prototype.addForce = function(force) {
+	this.velocity.x += force.x / this.mass;
+	this.velocity.y += force.y / this.mass;
 }
